6edde54cdaa3813bb28f9dc9c594bc1cbae6e6c8
maggie <maggie.tamanini@gmail.com>
2021-06-10 21:51:25 -0400

    More methods... so many more methods.  So much GetContinue. Wow.

--- GetToKnowClassmates/Program.cs
+++ GetToKnowClassmates/Program.cs
@@ -9,7 +9,23 @@ namespace GetToKnowClassmates
     {
         static void Main(string[] args)
         {
+            GetStudentListLoop();
 
+                if (GetContinue($"Would you like to add a student to the list? (Y/N)"))
+                {
+                    AddStudent();
+                    GetContinue("Would you like to add another student? (Y/N)");
+                }
+
+                if (GetContinue("Would you like to see the student list and learn about the students? (Y/N)"))
+                {
+                    GetStudentListLoop();
+                }
+
+            Console.WriteLine("Thanks for playing!");
+        }
+        public static void GetStudentListLoop()
+        {
             string filePath = @"Students.txt";
             StreamReader reader = new StreamReader(filePath);
 
@@ -27,7 +43,6 @@ namespace GetToKnowClassmates
                     allStudents.Add(newSt);
                 }
             }
-
             Console.WriteLine("These are the students in our class.\n");
             bool goOn = true;
             while (goOn == true)
@@ -37,70 +52,50 @@ namespace GetToKnowClassmates
                     Console.WriteLine($"{i}: {allStudents[i].Name}");
                 }
 
-                string number = GetUserInput("\nWhich student would you like to learn about?");
-                try
+                int index = GetUserIntInput("\nWhich student would you like to learn about?");
+
+
+                if (index > allStudents.Count || index < 0)
                 {
-                    int index = int.Parse(number);
+                    index = GetUserIntInput("That is not a valid number, please try again.");
+                }
 
-                    bool moreInfo = true;
-                    while (moreInfo == true)
+                bool moreInfo = true;
+                while (moreInfo == true)
+                {
+
+                    try
                     {
-
-                        try
+                        string additional = GetUserInput($"What additional information would you like about {allStudents[index].Name}? (Hometown, State, FavoriteFood)");
+                        if (additional == "hometown")
                         {
-                            string additional = GetUserInput($"What additional information would you like about {allStudents[index].Name}? (Hometown, State, FavoriteFood)");
-                            if (additional == "hometown")
-                            {
-                                Console.WriteLine($"{allStudents[index].Name}'s hometown is {allStudents[index].HomeTown}.");
-                            }
-                            else if (additional == "favoritefood")
-                            {
-                                Console.WriteLine($"{allStudents[index].Name}'s favorite food is {allStudents[index].FavoriteFood}.");
-                            }
-                            else if (additional == "state")
-                            {
-                                Console.WriteLine($"{allStudents[index].Name}'s state is {allStudents[index].State}.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("I don't understand, please try again.");
-                                break;
-                            }
-
-                            moreInfo = GetContinue($"Would you like to know more?");
+                            Console.WriteLine($"Hometown: {allStudents[index].HomeTown}");
                         }
-                        catch
+                        else if (additional == "favoritefood")
                         {
-                            Console.WriteLine("I don't understand, please start over.");
+                            Console.WriteLine($"Favorite food: {allStudents[index].FavoriteFood}");
                         }
+                        else if (additional == "state")
+                        {
+                            Console.WriteLine($"State: {allStudents[index].State}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("I don't understand, please try again.\n");
+                            continue;
+
+                        }
+
+                        moreInfo = GetContinue($"Would you like to know more? (Y/N)");
                     }
-                    goOn = GetContinue("Would you like to ask about someone else? (Y/N)");
-
-
-                }
-                catch
-                {
-                    Console.WriteLine("I don't understand, please start over.\n");
-                }
-
-                bool AddAnother = true;
-                while (AddAnother == true)
-                {
-                    reader.Close();
-                    string addStudent = (GetUserInput($"Would you like to add a student to the list? (Y/N)"));
-                    if (addStudent == "y")
+                    catch
                     {
-                        AddStudent();
-                        GetContinue("Would you like to add another student?");
-                       
+                        Console.WriteLine("I don't understand, please start over.");
                     }
-                    else
-                    {
-                        Console.WriteLine("Goodbye!");
-                    }
-                    AddAnother = false;
                 }
+                goOn = GetContinue("Would you like to ask about someone else? (Y/N)");
             }
+            reader.Close();
         }
 
         public static string ObjectToString(Student s)
@@ -135,7 +130,7 @@ namespace GetToKnowClassmates
 
 
             StreamWriter writer = new StreamWriter(filePath);
-            writer.Write($"{original}\n{line}");
+            writer.Write(original + line);
 
             writer.Close();
         }
@@ -165,52 +160,39 @@ namespace GetToKnowClassmates
         public static string GetUserInput(string message)
         {
             Console.WriteLine(message);
-            string input = Console.ReadLine().ToLower();
-            return input;
+            return Console.ReadLine().ToLower();
+
+        }
+
+        public static int GetUserIntInput(string message)
+        {
+            try
+            {
+                Console.WriteLine(message);
+                return int.Parse(Console.ReadLine().ToLower());
+            }
+            catch
+            {
+                return GetUserIntInput("That is not a valid number, please try again.");
+            }
         }
 
         public static bool GetContinue(string a)
         {
             Console.WriteLine(a);
-            string input = Console.ReadLine();
-            if (input.ToLower() == "y")
+            if (Console.ReadLine().ToLower() == "y")
             {
                 return true;
             }
-            else if (input.ToLower() == "n")
+            else if (Console.ReadLine().ToLower() == "n")
             {
                 return false;
             }
             else
             {
-                //Console.WriteLine("I don't understand that input. Please try again.");
                 return GetContinue("I don't understand that input. Please try again.");
             }
         }
-
-        //public static bool GetMoreInfo(string would)
-        //{
-        //    //Console.WriteLine($"Would you like to know more?");
-        //    string input = Console.ReadLine().ToLower();
-        //    try
-        //    {
-
-        //        if (input.ToLower() == "y")
-        //        {
-        //            return true;
-        //        }
-        //        else if (input.ToLower() == "n")
-        //        {
-        //            return false;
-        //        }
-        //        return GetMoreInfo();
-        //    }
-        //    catch
-        //    {
-        //        Console.WriteLine("I don't understand that input. Please try again.");
-        //        return GetMoreInfo();
-        //    }
-        //}
     }
 
 }
